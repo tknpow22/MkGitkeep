f9d8b84719491f57a3fb6e96c6cc60f9eeeebbee
tknpow22 <tknpow22@koutou-software.co.jp>
2014-03-12 04:46:29 +0900

    バージョン 1.0.0.3

---
+++ ResourceStringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace MkGitkeep {
+    [MarkupExtensionReturnTypeAttribute(typeof(string))]
+    class ResourceStringExtension : MarkupExtension {
+
+        string key;
+
+        public ResourceStringExtension(string key) {
+            this.key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            if (string.IsNullOrEmpty(this.key)) {
+                return "";
+            }
+            return Properties.Resources.ResourceManager.GetString(key) ?? "";
+        }
+    }
+}
